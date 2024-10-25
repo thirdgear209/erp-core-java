df85e0b65502a9f22ba82cbb56377be07e244c01
matthew dalby <matthew.dalby@g3enterprises.com>
2024-10-25 10:14:31 -0700

    Iniital support for messaging

--- src/main/java/com/thirdgear/example/erp_api_demo/core/controller/GlobalExceptionHandler.java
+++ src/main/java/com/thirdgear/example/erp_api_demo/core/controller/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.thirdgear.example.erp_api_demo.core.constants.ErrorConstants;
+import com.thirdgear.example.erp_api_demo.core.exception.InvalidRequestException;
 import com.thirdgear.example.erp_api_demo.core.service.ExceptionHandlerService;
 
 @ControllerAdvice
@@ -16,12 +17,11 @@ public class GlobalExceptionHandler {
 	@Autowired 
 	ExceptionHandlerService exceptionService;
 	
-	/*
-	@ExceptionHandler(CustomException.class)
-    public ResponseEntity<String> handleCustomException(CustomException ex) {
+	@ExceptionHandler(InvalidRequestException.class)
+    public ResponseEntity<String> handleCustomException(InvalidRequestException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
-	*/
+	
 	
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorMsg> handleGlobalException(Exception ex) {
@@ -33,6 +33,5 @@ public class GlobalExceptionHandler {
     	
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorMsg(ErrorConstants.SYSTEM_ERROR, "An system exception ocurred"));    
     }
-    
     
 }
